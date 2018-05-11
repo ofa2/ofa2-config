89e1b2c1671355155c6943b48963f264e953b754
xinshangshangxin <xinshangshangxin@gmail.com>
2018-05-11 09:41:50 +0800

    chore: remove build uglify

--- dist/bundle.esm.js
+++ dist/bundle.esm.js
@@ -1,2 +1,38 @@
-import _ from"lodash";import filePathOneLayer from"file-path-one-layer";import path from"path";function lift(){let t=path.join(this.projectPath,"config");return this.config={paths:{projectPath:this.projectPath,config:t,envConfig:path.join(t,"env")}},filePathOneLayer(t).then(t=>{_.forEach(t,t=>{let e=require(t.path);return _.merge(this.config,{[t.name]:e.default?e.default:e})})}).then(()=>{let t=path.join(this.config.paths.envConfig,this.environment),e=require(t);_.merge(this.config,e.default?e.default:e)}).then(()=>this)}export default lift;
+import _ from 'lodash';
+import filePathOneLayer from 'file-path-one-layer';
+import path from 'path';
+
+function lift() {
+  let configPath = path.join(this.projectPath, 'config');
+  this.config = {
+    paths: {
+      projectPath: this.projectPath,
+      config: configPath,
+      envConfig: path.join(configPath, 'env')
+    }
+  };
+  /* eslint-disable global-require */
+
+  /* eslint-disable import/no-dynamic-require */
+
+  return filePathOneLayer(configPath).then(files => {
+    _.forEach(files, file => {
+      let config = require(file.path);
+
+      return _.merge(this.config, {
+        [file.name]: config.default ? config.default : config
+      });
+    });
+  }).then(() => {
+    let envPath = path.join(this.config.paths.envConfig, this.environment);
+
+    let config = require(envPath);
+
+    _.merge(this.config, config.default ? config.default : config);
+  }).then(() => {
+    return this;
+  });
+}
+
+export default lift;
 //# sourceMappingURL=bundle.esm.js.map
